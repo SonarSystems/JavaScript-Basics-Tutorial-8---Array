c589e1d8bf0e7e72ef81c3c391e4baa33b493761
Sonar Systems <admin@sonarsystems.co.uk>
2014-04-20 09:33:12 +0100

    JavaScript Arrays

---
+++ main.js
@@ -0,0 +1,13 @@
+var myArr = new Array();
+
+myArr[0] = new Array(4);
+myArr[1] = new Array(5);
+myArr[2] = 78;
+
+myArr[0][2] = 8;
+
+alert(myArr[0][2]);
+
+alert(myArr[0]);
+alert(myArr[1]);
+alert(myArr[10]);
